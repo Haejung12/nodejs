5cb480e9b6fe1fd085e4e0734bc6d5ed82c5e70b
Haejung12 <59379358+Haejung12@users.noreply.github.com>
2020-01-02 17:03:15 +0900

    Add files via upload

---
+++ 01_basic/loop.js
@@ -0,0 +1,44 @@
+/*for(let i =0;i<5;i++) {
+    for(let k =0;k<=i;k++) {
+        process.stdout.write('*');
+    }
+    console.log()
+}*///삼각형별
+
+
+
+//역삼각형별
+/*
+for(let i =0;i<5;i++) {
+    for(let k=0;k<i;k++) {
+        process.stdout.write(' ');
+    }
+    
+    for(let k =5;k>i;k--) {
+        process.stdout.write('*');
+    }
+    console.log()
+}
+*/
+
+
+for(let i =0;i<4;i++) {
+    for(let k=0;k<3-i;k++) {
+        process.stdout.write(' ');
+    }
+    
+    for(let z =0;z<(i*2+1);z++) {
+        process.stdout.write('*');
+    }
+    console.log()
+}
+for(let i =0;i<3;i++) {
+    for(let k=0;k<i+1;k++) {
+        process.stdout.write(' ');
+    }
+    
+    for(let z =1;z<=((3-i)*2-1);z++) {
+        process.stdout.write('*');
+    }
+    console.log()
+}
