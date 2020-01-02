1464484228b05648cad78cb01692eed84c4b4f29
Haejung Kim <qqwsd57@naver.com>
2020-01-02 17:01:24 +0900

    nodejs

---
+++ 01_basic/callback.js
@@ -0,0 +1,15 @@
+function callFiveTimes(callback)  {
+    for(let i=0; i<5;i++)  {
+        callback();
+    }
+}
+
+callFiveTimes(function()  {
+    console.log('callback function')
+
+});
+
+let anonymous = function()  {
+    console.log('callback function')
+}
+callFiveTimes(anonymous);
